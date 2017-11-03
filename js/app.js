266fd5ba20e516330148306acf8bc4892debc382
Shadeira <shadeiranes@gmail.com>
2017-11-02 21:24:48 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -97,7 +97,7 @@ var Player = function() {
 // Check if playerLives is 0, if so call reset
   Player.prototype.update = function() {
     if (this.playerLives === 0){
-     reset();
+     this.characterReset();
     }
   
   
